4ede50cbc7287654170a18614c931ed6043ac909
rajputshashank003 <hariomshashank@gmail.com>
2025-04-21 01:05:10 +0530

    update resume link

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -6,7 +6,7 @@ export const data = {
     "github" : "https://github.com/rajputshashank003",
     "project_repo" : "https://github.com/rajputshashank003/details",
     "twitter_link" : "https://x.com/_rajputshashank",
-    "resume_link" : "https://drive.google.com/file/d/18R-JVelgNuuEPizehRnNXo0wfcUGz6Ba/view?usp=sharing",
+    "resume_link" : "https://drive.google.com/file/d/1pqFFmp8t2EQ9TBIRw6_m7kDNEeZ9Ej7t/view?usp=sharing",
     "about_me_data_1" : "Hi, I’m Shashank Rajput, currently pursuing a Bachelor's degree in Computer Science and Engineering at Chitkara University.",
     "about_me_data_2" : "I am passionate about solving complex problems using data structures, algorithms, and leveraging various technologies such as Java, JavaScript, and the MERN stack.    ",
 }
