c8c3240d386824dbe0dc417c7a0e5efd74d30783
rajputshashank003 <hariomshashank@gmail.com>
2025-07-27 04:29:45 +0530

    feat: update resume

--- src/components/RedCarModel.tsx
+++ src/components/RedCarModel.tsx
@@ -1,4 +1,4 @@
-import { Environment } from "@react-three/drei"
+import { Environment, OrbitControls } from "@react-three/drei"
 import { PlaneCar } from "../../public/RedCar"
 import { Canvas } from "@react-three/fiber"
 
@@ -6,11 +6,18 @@ const RedCarModel = () => {
   return (
     <Canvas camera={{ position: [40.81179373106988,19.674589892349776,43.969398506466185], fov: 50 }}
         gl={{ antialias: true, alpha: true, pixelRatio: Math.min(window.devicePixelRatio, 2) }}
-        style={{ background: 'transparent', pointerEvents : "none" , width : "100%" }}
+        style={{ background: 'transparent', width : "100%" }}
     >
         <ambientLight intensity={2} />
         <Environment preset="park" />
         <PlaneCar scale={0.16} />
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={0}
+          target={[0, 0, 0]}
+          enableRotate={true}
+        />
         <directionalLight position={[10, 10, 10]} intensity={2} castShadow />
         <directionalLight position={[0, 150 , 10]} intensity={2} castShadow />
     </Canvas>
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -6,7 +6,7 @@ export const data = {
     "github" : "https://github.com/rajputshashank003",
     "project_repo" : "https://github.com/rajputshashank003/details",
     "twitter_link" : "https://x.com/_rajputshashank",
-    "resume_link" : "https://drive.google.com/file/d/1pqFFmp8t2EQ9TBIRw6_m7kDNEeZ9Ej7t/view?usp=sharing",
-    "about_me_data_1" : "Hi, I’m Shashank Rajput, currently pursuing a Bachelor's degree in Computer Science and Engineering at Chitkara University.",
-    "about_me_data_2" : "I am passionate about solving complex problems using data structures, algorithms, and leveraging various technologies such as Java, JavaScript, and the MERN stack.    ",
+    "resume_link": "https://drive.google.com/file/d/1n8D-DIarsi-PJol_ed0thc5Tv5G0QdSa/view?usp=drive_link",
+    "about_me_data_1" : "Hi, I’m Shashank Rajput, currently working as Software Engineer intern at WizCommerce.",
+    "about_me_data_2" : "I am passionate about solving complex problems using data structures, algorithms, and leveraging various technologies such as Java, TypeScript, MERN stack etc.    ",
 }
