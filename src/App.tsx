636702d4c93b4cbb9ad4b219caa4338f7c01f143
rajputshashank003 <hariomshashank@gmail.com>
2025-07-30 23:19:58 +0530

    feat: add github as option & fix: styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Footer from './components/Footer'
 import { BrowserRouter } from "react-router-dom";
 import RoutesComponent from './components/RoutesComponent';
+import { useEffect } from 'react';
+import LocomotiveScroll from 'locomotive-scroll';
 
 const App = () => {
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+    return () => locomotiveScroll.destroy();
+  }, []);
+
   return (
     <BrowserRouter >
       <div className='h-fit w-screen relative bg-black'>
--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion"
 import { Link, useNavigate } from "react-router-dom"
 
-const Options = ({link , name, local } : {link : string , name : string, local : boolean}) => {
+const Options = ({link , name, local, count = 1 } : {link : string , name : string, local : boolean, count?: number}) => {
     const navigate = useNavigate();
     const handleClick = (e : any) => {
         if (local) {
@@ -12,6 +12,8 @@ const Options = ({link , name, local } : {link : string , name : string, local :
     return (
     <Link to={local ? "#" : link} onClick={handleClick}>
         <motion.div 
+            data-scroll
+            data-scroll-speed={count % 2 ? '-0.03' : '0.03'}
             whileHover={{
                 rotateX : 25,
                 rotateY : 10,
@@ -21,13 +23,13 @@ const Options = ({link , name, local } : {link : string , name : string, local :
                 duration : 0.3,
                 ease : "easeInOut"
             }}
-            className="hover:text-neutral-200 [perspective::1000px] [transform-style:preserve-3d] group relative font-medium font-jetbrains transform cursor-pointer duration-200 hover:text-[2.5rem] h-20 w-fit max-w-38"
+            className="hover:text-neutral-200 shadow-[1px_-1px_2px] shadow-gray-300 bg-gray-400/20 hover:bg-gray-400/10 p-2 px-4 h-fit rounded-[12px]  backdrop-blur-2xl [perspective::1000px] [transform-style:preserve-3d] group relative font-medium font-jetbrains transform cursor-pointer duration-200 max-sm:hover:text-[12px] hover:text-[2.5rem] w-fit"
         >
-            <span className="group-hover:text-[4rem] duration-200">{name[0]}</span>
+            <span className="max-sm:group-hover:text-[28px] group-hover:text-[4rem] duration-200">{name[0]}</span>
             {name.slice(1)}
-            <span className="h-[1.1px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 left-0 w-full bg-gradient-to-r from-transparent via-yellow-500 to-transparent"></span>
-            <span className="h-[5px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 left-0 w-full bg-gradient-to-r blur-sm from-transparent via-yellow-500 to-transparent"></span>
-            <span className="h-[2px] left-[30%] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 left-0 w-1/2 bg-gradient-to-r blur-sm from-transparent via-yellow-300 to-transparent"></span>
+            <span className="h-[1.1px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 left-0 w-full bg-gradient-to-r from-transparent via-blue-500 to-transparent"></span>
+            <span className="h-[5px] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 left-0 w-full bg-gradient-to-r blur-sm from-transparent via-blue-500 to-transparent"></span>
+            <span className="h-[2px] left-[30%] absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 bottom-0 w-1/2 bg-gradient-to-r blur-sm from-transparent via-blue-300 to-transparent"></span>
         </motion.div> 
     </Link>
     )
